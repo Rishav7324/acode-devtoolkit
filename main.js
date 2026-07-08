e2d408a60fdb6b56ad521277fcae535b59f5eec4
Rishav7324 <rishavraj74883@gmail.com>
2026-07-08 14:19:41 +0530

    feat: add plugin lifecycle

--- main.js
+++ main.js
@@ -1,30 +1,51 @@
-class DevToolkit {
-  async init() {
-    acode.alert(
-      "Acode DevToolkit",
-      "🎉 Plugin successfully loaded!\n\nWelcome to Acode DevToolkit."
-    );
-  }
+const PLUGIN_ID = "com.rishav.devtoolkit";
 
-  async destroy() {
-    console.log("Acode DevToolkit unloaded");
+let commands;
+
+function init(baseUrl, $page, cache) {
+  commands = acode.require("commands");
+
+  commands.addCommand({
+    name: "devtoolkit.open",
+    description: "Open Acode DevToolkit",
+    exec: () => {
+      $page.innerHTML = `
+        <div style="padding:16px;">
+          <h2>🚀 Acode DevToolkit</h2>
+          <p>Welcome to the first version of Acode DevToolkit.</p>
+
+          <hr>
+
+          <h3>Available Modules</h3>
+
+          <ul>
+            <li>🆔 UUID Generator (Coming Soon)</li>
+            <li>📄 JSON Formatter (Coming Soon)</li>
+            <li>🔤 Base64 Tools (Coming Soon)</li>
+          </ul>
+
+          <p><b>Version:</b> 0.1.0</p>
+        </div>
+      `;
+
+      $page.show();
+    }
+  });
+
+  acode.pushNotification(
+    "Acode DevToolkit",
+    "Plugin loaded successfully!",
+    {
+      type: "success"
+    }
+  );
+}
+
+function unmount() {
+  if (commands) {
+    commands.removeCommand("devtoolkit.open");
   }
 }
 
-if (window.acode) {
-  const plugin = new DevToolkit();
-
-  acode.setPluginInit(
-    "com.rishav.devtoolkit",
-    async () => {
-      await plugin.init();
-    }
-  );
-
-  acode.setPluginUnmount(
-    "com.rishav.devtoolkit",
-    async () => {
-      await plugin.destroy();
-    }
-  );
-}
+acode.setPluginInit(PLUGIN_ID, init);
+acode.setPluginUnmount(PLUGIN_ID, unmount);
